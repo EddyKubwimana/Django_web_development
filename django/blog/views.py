ff0714d0fa4ab0317d24a3d577320e6ff517921f
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-15 23:50:10 +0000

    Add files via upload

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.4
 
-def Home(request):
-    return HttpResponse('<h1>Home</h1>')
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
-def News(request):
-    return HttpResponse('<h1>News</h1>')
-def Published(request):
-    return HttpResponse('<h1>Published papers</h1>')
-def Editors(request):
-    return HttpResponse('<h1> Editors</h1>')
-def About(request):
-    return HttpResponse('<h1>About</h1>')
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+def posts(request):
+    return render( request, 'blog/posts.html')
 
 
 
