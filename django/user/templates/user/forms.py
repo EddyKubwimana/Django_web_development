b4afa7045c8183eda0358e9b1732d3fec38e1da9
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-13 16:27:24 +0000

    Add files via upload

--- django/user/templates/user/forms.py
+++ django/user/templates/user/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField
+    email = forms.EmailField()
     class Meta:
         model = User
         fields = ["username", "password1", "password2","email"]
- 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
