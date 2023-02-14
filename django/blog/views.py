1471b1e2103cf545375c37c33a591c2ce31c10ab
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-14 21:12:58 +0000

    Add files via upload

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
@@ -26,8 +14,33 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
-def posts(request):
-    return render( request, 'blog/posts.html')
+def post(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/post.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detailview.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/createpost.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 
 
 
